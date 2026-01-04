6fccf39c57cfa800ca273e177f2bf8046e4afa37
Suyog Nimbre <heisenbergnimbre@gmail.com>
2026-01-04 16:59:49 +0530

    Replaced Google search test with stable demo login flow to avoid CAPTCHA

--- src/test/java/GoogleSearchTest.java
+++ src/test/java/GoogleSearchTest.java
@@ -2,10 +2,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class GoogleSearchTest {
 
@@ -37,6 +41,32 @@ public class GoogleSearchTest {
 
 //        Assert.assertTrue(title.contains("Selenium WebDriver"),
 //                "Search did not work as expected");
+    }
+
+    @Test
+
+    public void loginWithValidCredentials(){
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("username")))
+                .sendKeys("tomsmith");
+
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("flash")));
+
+        String successMsg = driver.findElement(By.id("flash")).getText();
+
+        Assert.assertTrue(successMsg.contains("You logged into a secure area"),"Login was not successful");
+
+
+
+
+
     }
 
     @AfterMethod
