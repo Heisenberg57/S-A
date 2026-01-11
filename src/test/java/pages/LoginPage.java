6f930246f95fe208fd694e5e5465e19b46ca6e21
Suyog Nimbre <heisenbergnimbre@gmail.com>
2026-01-11 16:25:07 +0530

    Refactored LoginPage to use reusable wait utilities

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,9 +2,11 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import utils.WaitUtils;
 
 public class LoginPage {
     WebDriver driver;
+    WaitUtils wait;
     private static final String LOGIN_PATH = "/login";
 
     //Locators - UI info here
@@ -17,22 +19,30 @@ public class LoginPage {
     public LoginPage(WebDriver driver,String baseUrl){
         this.driver=driver;
         driver.get(baseUrl+LOGIN_PATH);
+        this.wait = new WaitUtils(driver);
     }
 
     //Page Actions(business logic)
     public void enterUsername(String username){
-        driver.findElement(usernameInput).sendKeys(username);
+
+       // driver.findElement(usernameInput).sendKeys(username);
+        wait.waitForVisibility(usernameInput).sendKeys(username);
     }
 
     public void enterpassword(String password){
-        driver.findElement(passwordInput).sendKeys(password);
+
+        //driver.findElement(passwordInput).sendKeys(password);
+        wait.waitForVisibility(passwordInput).sendKeys(password);
     }
 
     public void clickLogin(){
-        driver.findElement(loginButton).click();
+
+        //driver.findElement(loginButton).click();
+        wait.waitForClickability(loginButton).click();
     }
 
     public String getFlashMessage(){
-        return driver.findElement(flashMessage).getText();
+        //return driver.findElement(flashMessage).getText();
+        return wait.waitForVisibility(flashMessage).getText();
     }
 }
