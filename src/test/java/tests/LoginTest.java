4230fddba2e82c256cd48c74160f329bf9641187
Suyog Nimbre <heisenbergnimbre@gmail.com>
2026-01-12 22:23:42 +0530

    Implemented this listener in Test

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import base.BaseTest;
+import listeners.TestListener;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
@@ -8,9 +9,11 @@ import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 
+@Listeners(TestListener.class)
 public class LoginTest extends BaseTest {
 
 
@@ -23,8 +26,10 @@ public class LoginTest extends BaseTest {
         loginPage.clickLogin();
 
         String message = loginPage.getFlashMessage();
-        Assert.assertTrue(message.contains("You logged into a secure area"),
-                "Login was not successful");
+//        Assert.assertTrue(message.contains("You logged into a secure area"),
+//                "Login was not successful");
+        Assert.assertTrue(false, "Forcing failure to test screenshot");
+
 
 
     }
