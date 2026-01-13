6beb24fd9163a2283ca546401bba18934007e2d8
Suyog Nimbre <heisenbergnimbre@gmail.com>
2026-01-13 22:24:10 +0530

    Introduced DriverFactory to centralize WebDriver creation

---
+++ src/test/java/factory/DriverFactory.java
@@ -0,0 +1,23 @@
+package factory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+
+    public static WebDriver createDriver(String browser){
+
+        if(browser == null){
+            browser = "chrome";
+        }
+
+        switch (browser.toLowerCase()){
+            case "firefox":
+                return new FirefoxDriver();
+            case "chrome":
+            default:
+                return new ChromeDriver();
+        }
+    }
+}
