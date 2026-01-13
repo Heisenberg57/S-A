cc7f4436fd90c8d243216b3a3101a895314698d2
Suyog Nimbre <heisenbergnimbre@gmail.com>
2026-01-13 22:36:30 +0530

    Refactored BaseTest to use DriverFactory for browser initialization

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,5 +1,6 @@
 package base;
 
+import factory.DriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -11,7 +12,10 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp(){
-        driver = new ChromeDriver();
+//        driver = new ChromeDriver();
+//        driver.manage().window().maximize();  old hard coded setup for driver
+
+        driver= DriverFactory.createDriver("chrome");
         driver.manage().window().maximize();
     }
 
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -26,9 +26,9 @@ public class LoginTest extends BaseTest {
         loginPage.clickLogin();
 
         String message = loginPage.getFlashMessage();
-//        Assert.assertTrue(message.contains("You logged into a secure area"),
-//                "Login was not successful");
-        Assert.assertTrue(false, "Forcing failure to test screenshot");
+        Assert.assertTrue(message.contains("You logged into a secure area"),
+                "Login was not successful");
+//        Assert.assertTrue(false, "Forcing failure to test screenshot");
 
 
 
