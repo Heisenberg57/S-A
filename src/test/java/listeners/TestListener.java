c8da657ff1e4e0bef1e36e96bcf4ededf3677420
Suyog Nimbre <heisenbergnimbre@gmail.com>
2026-01-20 21:34:02 +0530

    Integrated Extent Reports for test execution reporting

--- src/test/java/listeners/TestListener.java
+++ src/test/java/listeners/TestListener.java
@@ -1,24 +1,70 @@
 package listeners;
 
 import base.BaseTest;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import jdk.jfr.internal.consumer.OngoingStream;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import reporting.ExtentManager;
 import utils.ScreenshotUtils;
 
 public class TestListener implements ITestListener {
+
+//    @Override
+//    public void onTestFailure(ITestResult result){
+//        Object testClass = result.getInstance();
+//
+//        if (testClass instanceof BaseTest){
+//            BaseTest baseTest = (BaseTest) testClass;
+//
+//            ScreenshotUtils.takeScreenshot(
+//                    baseTest.getDriver(),
+//                    result.getMethod().getMethodName()
+//
+//            );
+
+    private static ExtentReports extent = ExtentManager.getInstance();
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onTestStart(ITestResult result){
+        ExtentTest extentTest = extent.createTest(result.getMethod().getMethodName());
+        test.set(extentTest);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result){
+        test.get().pass("Test Passed");
+    }
 
     @Override
     public void onTestFailure(ITestResult result){
         Object testClass = result.getInstance();
 
-        if (testClass instanceof BaseTest){
+        if(testClass instanceof BaseTest){
             BaseTest baseTest = (BaseTest) testClass;
 
-            ScreenshotUtils.takeScreenshot(
+            String screenshotPath = ScreenshotUtils.takeScreenshot(
                     baseTest.getDriver(),
                     result.getMethod().getMethodName()
-
             );
+
+            test.get().fail(result.getThrowable());
+
+            if(screenshotPath!=null){
+                test.get().addScreenCaptureFromPath(screenshotPath);
+            }
+
+
+
+
         }
     }
+
+    @Override
+    public void onFinish(ITestResult result){
+        extent.flush();
+    }
 }
+
